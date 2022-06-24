e38945f5dd0538ee0f5ce4f03f2886a1380403e6
eskander-taher <eskander.alshaibany@gmail.com>
2022-06-24 23:10:09 +0500

    first React App: React facts single page

---
+++ first-react-page/react-facts/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import MainContent from "./components/MainContent";
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <MainContent />
+        </div>
+    )
+}
